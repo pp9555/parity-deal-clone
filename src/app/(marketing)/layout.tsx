7c957fb7b1dada8aaa1e1457c145946be909b640
pp9555 <160492858+pp9555@users.noreply.github.com>
2025-05-14 15:02:19 +0530

    first commit

---
+++ src/app/(marketing)/layout.tsx
@@ -0,0 +1,14 @@
+import { NavBar } from "./_components/Navbar";
+
+
+export default function MarketingLayout({
+  children
+}:{children: React.ReactNode} ) {
+  return (
+    <div className="selection:bg-[hsl(320,65%,52%,20%)]">
+        <NavBar />
+        jwrwhrfifjrf
+      {children}
+    </div>
+  );
+}
